2d737540195b987e10f069c8357d166b66f0cefa
kreuzerk <kevin.kreuzer90@icloud.com>
2023-06-11 20:46:15 +0200

    feat: 🎸 share state between remote and host

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {UserService} from "@nx-dynamic-mf/shared/data-access-user";
+import {Router} from "@angular/router";
+import {distinctUntilChanged} from "rxjs";
 
 @Component({
   selector: 'nx-dynamic-mf-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="dashboard-nav">Admin Dashboard</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `,
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  private userService = inject(UserService);
+  private router = inject(Router);
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        // Queue the navigation after initialNavigation blocking is completed
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
+
 }
